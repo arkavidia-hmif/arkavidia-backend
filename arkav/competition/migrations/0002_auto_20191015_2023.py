7e266a4901dedb9cec3bfe1e747c9c1ed92787e1
Jones Napoleon <jonesnapoleonautumn1@gmail.com>
2019-10-16 10:20:30 +0700

    final announcemnt, test submit task

--- arkav/competition/migrations/0002_auto_20191015_2023.py
+++ arkav/competition/migrations/0002_auto_20191015_2023.py
@@ -24,7 +24,10 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.CharField(max_length=1000)),
                 ('date_sent', models.DateTimeField(default=datetime.datetime.now)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='announcements', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, 
+                    related_name='announcements', 
+                    to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
