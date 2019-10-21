e36489bd6918ea33c030cf0e959b15e19dd396ef
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-10-21 09:42:30 +0700

    add whitenoise

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ FROM python:3.6-slim
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+EXPOSE 8000
+RUN pip install pipenv
+
 WORKDIR /code
 COPY . /code
 
-RUN pip install pipenv
 RUN pipenv install --system
-
-EXPOSE 8000
 
 CMD ["/code/start.sh"]
--- arkav/settings.py
+++ arkav/settings.py
@@ -57,6 +57,7 @@ AUTH_USER_MODEL = 'arkavauth.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
