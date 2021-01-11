a9545c4babf1fbebf04eee730c87a8d70677658a
Asif Hummam Rais <hashshura@gmail.com>
2021-01-12 01:12:55 +0700

    Change datetime.datetime.now() into django.utils.timezone.now()

--- arkav/cron.py
+++ arkav/cron.py
@@ -1,11 +1,11 @@
 from arkav.mainevent.models import Registrant
-from datetime import datetime
 from datetime import timedelta
+from django.utils import timezone
 from django.db.models import Count
 
 
 def delete_inactive_registrants():
-    yesterday = datetime.now() - timedelta(days=1)
+    yesterday = timezone.now() - timedelta(days=1)
     inactive_registrants = Registrant.objects \
         .annotate(task_responses_count=Count('task_responses')) \
         .filter(task_responses_count=0, created_at__lte=yesterday)
